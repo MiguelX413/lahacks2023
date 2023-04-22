e138bd996ff09b9e7fc57bdb5da34072c0d715e4
MiguelX413 <lbpgaming27@gmail.com>
2023-04-22 16:07:26 +0000

    Make basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::NamedFile;
+use actix_web::{get, web, App, HttpRequest, HttpServer};
+use std::path::PathBuf;
+
+#[get("/")]
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path = ["static", "index.html"].into_iter().collect::<PathBuf>();
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
